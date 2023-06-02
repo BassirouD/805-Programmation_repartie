a8ee089851dc8bdd2b92e843da5c30ee136cce69
BassirouD <moubass.diallo@gmail.com>
2023-06-02 19:57:04 +0200

    Login puis redirection vers acceuil

--- Tracking-JEE/src/java/org/urca/Beans/LoginBean.java
+++ Tracking-JEE/src/java/org/urca/Beans/LoginBean.java
@@ -1,6 +1,10 @@
 package org.urca.Beans;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,6 +13,9 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.urca.utils.UtilsConnexion;
 
 @ManagedBean
@@ -16,17 +23,26 @@ import org.urca.utils.UtilsConnexion;
 public class LoginBean {
     private String email;
     private String password;
-    private String message;
+    private String msg;
+    private String nom;
 
-    public String getMessage() {
-        return message;
+    public String getNom() {
+        return nom;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setNom(String nom) {
+        this.nom = nom;
     }
     
     
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 
     public String getEmail() {
         return email;
@@ -58,23 +74,64 @@ public class LoginBean {
             rs = preparedStatement.executeQuery();
             if (rs.next()) {
                 System.out.println("---------------------"+rs.getString(1));
-                // L'utilisateur est authentifié avec succès
-                // Effectuez les actions nécessaires, par exemple, enregistrer l'utilisateur dans la session
-                // et rediriger vers une autre page
+                int id = Integer.parseInt(rs.getString(1));
+                nom = rs.getString(5);
                 FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("utilisateur", email);
-                FacesMessage message1 = new FacesMessage(FacesMessage.SEVERITY_INFO, "Message succes", "Auth résussi.");
-                message = "Auth résussi.";
+                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Message succes", "Auth résussi." + nom);
+                msg = "Auth résussi." + id;
                 
+                ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+                HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+                
+                HttpSession session = request.getSession();
+                session.setAttribute("id", id);
+                session.setAttribute("nom", nom);
+                
+                int valeur = (int) session.getAttribute("id");
+                System.out.println("Session===>: " + id);
+                
+                externalContext.redirect("accueil.xhtml");
             } else {
-                message = "Identifiants invalides.";
+                msg = "Identifiants invalides.";
                 // L'authentification a échoué
-                FacesMessage message2 = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Identifiants invalides", "Veuillez vérifier votre nom d'utilisateur et votre mot de passe.");
-                FacesContext.getCurrentInstance().addMessage(null, message2);
+                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Identifiants invalides", "Veuillez vérifier votre nom d'utilisateur et votre mot de passe.");
+                FacesContext.getCurrentInstance().addMessage(null, message);
             }
         }catch(Exception e) {
             System.out.println("Error " + e.getMessage());
             FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erreur de base de données", "Une erreur s'est produite lors de la connexion à la base de données.");
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
+    }
+    
+     public void envoyerFormulaire() throws IOException {
+        // Accéder à la requête HTTP
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+        HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
+        
+        String contextPath = externalContext.getRequestContextPath();
+        // URL de la servlet
+        String servletURL = "http://localhost:8080/Tracking-JEE/Login";
+        URL url = new URL(servletURL);
+         System.out.println("============================"+url);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+ // Paramètres à envoyer à la servlet
+        String params = "email=" + email + "&password=" + password;
+
+        // Ajouter les données dans la requête
+        request.setAttribute("email", email);
+        request.setAttribute("password", password);
+        // Ajouter d'autres attributs du formulaire à la requête
+        
+        // Envoi des paramètres à la servlet
+        try (OutputStream outputStream = connection.getOutputStream()) {
+            byte[] postData = params.getBytes(StandardCharsets.UTF_8);
+            outputStream.write(postData);
+        }
+
+        
     }
 }
